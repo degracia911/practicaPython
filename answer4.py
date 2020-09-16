01c5c4e7a7d5dca32d3ef0699a3299b8980239ea
Ruddy De Gracia <ruddydegracia@gmail.com>
2020-09-16 17:33:12 -0400

    avance preguta 4

--- answer4.py
+++ answer4.py
@@ -1,5 +1,6 @@
+numero=100
+segundo_numero=50
+if 100 > 50  ==0:
+    print () 
 
-# test
-# expresion 
-primer numero > que el segundo numero
-print ()
+print ("el primer numerno es mayor que el segundo")
