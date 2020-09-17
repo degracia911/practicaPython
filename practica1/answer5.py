17b300391dcdc32846f4f709872621d880b3850c
Ruddy De Gracia <ruddydegracia@gmail.com>
2020-09-17 19:23:47 -0400

    cambio 5

---
+++ practica1/answer5.py
@@ -0,0 +1,9 @@
+# dolar a pesos
+cantidad_dolar= input ("ingrese: cantidad de dolares: ")
+cantidad_dolar= float (cantidad_dolar)
+tasa= 58.5
+cantidad_dolar= 100
+print (cantidad_dolar * tasa)
+print (cantidad_peso / tasa)
+
+
