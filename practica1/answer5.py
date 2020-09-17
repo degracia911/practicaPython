021ee578ab0f2453c98522432956b5ae1f3bf742
Ruddy De Gracia <ruddydegracia@gmail.com>
2020-09-17 19:48:45 -0400

    cambio peso

--- practica1/answer5.py
+++ practica1/answer5.py
@@ -1,9 +1,9 @@
-# dolar a pesos
-cantidad_dolar= input ("ingrese: cantidad de dolares: ")
-cantidad_dolar= float (cantidad_dolar)
-tasa= 58.5
-cantidad_dolar= 100
-print (cantidad_dolar * tasa)
-print (cantidad_peso / tasa)
+print ("conversor  \ n " )
+peso = int (input ("digite la cantidad en pesos" ))
+tasa= 58.50
+resultado= (dolar * peso)
+print ("total de peso es:" + str(resultado))
+
+
 
 
