9295be03747be425ad7ce7a56dad17ff3c21e1e7
Ruddy De Gracia <ruddydegracia@gmail.com>
2020-09-17 19:59:37 -0400

    cambio

--- practica III/answer6.py
+++ practica III/answer6.py
@@ -2,8 +2,8 @@ x = int (input ("digite su numero de cedula: "))
 def cedula ():
     if cedula ==11:
     print (" cedula es validar")
-
 else: 
     print ("cedula es invalida ")
     cedula()
+    
 
